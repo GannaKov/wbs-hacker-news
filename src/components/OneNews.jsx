3aeef857eb2b973fecc6f8be6487961723004e3e
GannaKov <zlatta2000@gmail.com>
2024-02-10 14:04:58 +0100

    time ago

--- src/components/OneNews.jsx
+++ src/components/OneNews.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable react/prop-types */
+import TimeAgo from "react-timeago";
 
 const OneNews = ({ item }) => {
   const { title, author, created_at, num_comments } = item;
+
   return (
     <div>
       <h2>{title}</h2>
       <div>
-        <p>{author}</p>
-        <p>{created_at}</p>
-        <p>{num_comments}</p>
+        <p>author: {author}</p>
+        {/* <p>date: {created_at}</p> */}
+        <TimeAgo date={created_at} />
+        <p>comments: {num_comments}</p>
       </div>
     </div>
   );
